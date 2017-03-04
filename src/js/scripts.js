a87357bd4ce56806d6c3c0ad1df1f484627f4130
Dehan <dehandecroos@gmail.com>
2017-03-04 17:38:28 +0530

    Settng up react router completed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./components/Layout";
+import Bootstrap from "bootstrap.native";
 
+import {Router, Route, IndexRoute, hashHistory} from "react-router";
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={Layout}>
+        
+        </Route>
+    </Router>
+, app);
